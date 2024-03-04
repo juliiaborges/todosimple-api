f844d1661f93c9ab97ed694327a7dcf41cae9e8f
Júlia Borges <juliaborgesfacul@gmail.com>
2024-03-04 12:07:14 -0300

    Criação do User Service

--- src/main/java/com/juliaborges/todosimple/services/UserService.java
+++ src/main/java/com/juliaborges/todosimple/services/UserService.java
@@ -21,18 +21,32 @@ public class UserService {
     private TaskRepository taskRepository;
 
     public User findById(Long id){
-         Optional<User> user = this.userRepository.findById(id);
-         return user.orElseThrow(()-> new RuntimeException(
+         Optional<User> user = this.userRepository.findById(id);//Optional -> Não recebe Null e sim um Vazio. This -> diz repeito a qual classe é 
+         return user.orElseThrow(()-> new RuntimeException( //->orElseThrow (Eu retorno o usuário se ele estiver preenchido ) ->RuntimeException (ele não para e se não estiver preenchido faça...)
             "Usuário não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
          ) ); 
     }
-    @Transactional
+    @Transactional //Utilizar sempre que fizermos uma inserção -> COnseguimos ter controle melhor do que está acontecendo na aplicação (Atomicidade)
     public User create(User obj){
       obj.setId(null);
       obj = this.userRepository.save(obj);
       this.taskRepository.saveAll(obj.getTasks());
       return obj;
+    }
 
+    public User update(User obj){
+       User newObj = findById(obj.getId());
+       newObj.setPassword(obj.getPassword());
+       return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+      findById(id);
+      try{
+        this.userRepository.deleteById(id);
+      } catch (Exception e){
+        throw new RuntimeException("Não é possível excluir, pois há tarefas relacionadas!");
+      }
     }
 
     }
